1f42fff4559ad84cd36d5f423bf829997f7bbed0
Denisha Garner <thegarnercode@gmail.com>
2022-04-17 13:03:03 -0400

    commit tree 92ecb6fdbb6ef74fc0cc940c918928ea93ff1a8f

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+//your solution here!
+
+//1
+const cats = ["Milo","Otis","Garfield" ]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+//2
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+function appendCat(name) { 
+  return  [...cats, name]
+}
+
+function prependCat(name){
+    return [name,...cats]
+}
+
+function removeLastCat(){
+const copyOfCats = [...cats]
+   copyOfCats.pop()
+   return copyOfCats;
+}
+
+function removeFirstCat(){
+    const copyOfCats = [...cats]
+    copyOfCats.shift()
+    return copyOfCats;
+}
+
+
